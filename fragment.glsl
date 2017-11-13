2c254ffb467c5b57e65b16e1c034f922f1dd6834
Samuel Pagenkopf <ssaammp@gmail.com>
2017-11-12 20:14:02 -0600

    cool circle shader (lol)

--- fragment.glsl
+++ fragment.glsl
@@ -1,10 +1,17 @@
 #version 120
 
-uniform vec2 win_size;
+uniform vec2 win_size, screen_size;
 uniform vec3 bg_color;
 
 void main (void)  
 {     
-	vec2 color_mod = gl_FragCoord.xy / win_size;
-	gl_FragColor = vec4(bg_color * color_mod.xxy, 1.0);
+	// Divide the position input into a number of steps equal to pixel res
+//	vec2 pixel_pos = floor(gl_FragCoord.xy / win_size * screen_size);
+	vec2 pixel_pos = gl_FragCoord.xy;
+	vec2 pos_quant = pixel_pos / screen_size;
+
+	vec2 off_pos = pixel_pos + bg_color.rg * 1000.0;
+	float circles = sin(off_pos.x / 10.0) * cos(off_pos.y / 10.0);
+
+	gl_FragColor = vec4(bg_color * pos_quant.xxy * circles, 1.0);
 }
