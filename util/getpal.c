5d91b6bab9a2a0fee113df6a32aa30fcbdb9a905
Samuel Pagenkopf <ssaammp@gmail.com>
2017-11-15 22:42:18 -0600

    made getpal mark unused colors

--- util/getpal.c
+++ util/getpal.c
@@ -1,17 +1,11 @@
 #include <SDL2/SDL.h>
 
-const int err(const char *msg) {
-	fprintf(stderr, "%s\n", msg);
-	return -1;
-}
+static int err(const char *msg);
+static void mark_unused(SDL_Surface* img, SDL_Color *colors);
+static void print_colors(SDL_Color *colors);
 
-unsigned col_to_rgb(SDL_Color c) {
-	return c.r << 16
-		| c.g << 8
-		| c.b;
-}
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 	if(argc<2)
 		return err("Please specify a BMP");
 	SDL_Surface *img = SDL_LoadBMP(argv[1]);
@@ -20,9 +14,37 @@ int main(int argc, char **argv) {
 	SDL_Palette *p = img->format->palette;
 	if(!p)
 		return err("Attempt to load image without color index");
-	for(int i=0; i<p->ncolors; ++i) {
-		printf("%06x ", col_to_rgb(p->colors[i]));
+
+	/* Copy into theoretical palette of 256 */
+	SDL_Color colors[256] = {0};
+	memcpy(colors, p->colors, p->ncolors * sizeof(SDL_Color));
+
+	mark_unused(img, colors);
+	print_colors(colors);
+
+	return 0;
+}
+
+static int err(const char *msg)
+{
+	fprintf(stderr, "%s\n", msg);
+	return -1;
+}
+
+void mark_unused(SDL_Surface *img, SDL_Color *colors) {
+	char used[256] = {0};
+	void *end = img->pixels + img->w * img->h;
+	for(char *col = img->pixels; col != end; ++col) {
+		used[*col] = 0xff;
+	}
+	for(int i=0; i<256; ++i) {
+		colors[i].a = used[i];
+	}
+}
+
+void print_colors(SDL_Color *colors) {
+	for(int i=0; i<256; ++i) {
+		printf("%08x ", *(unsigned*)(&colors[i]));
 		if(i%8==7) puts("");
 	}
-	return 0;
 }
